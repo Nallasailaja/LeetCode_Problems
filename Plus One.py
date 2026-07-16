120ac48bf9cab43b885be517c09cb9250a4b3b1a
Sailaja Nalla <145919073+Nallasailaja@users.noreply.github.com>
2026-07-16 21:03:43 +0530

    Create Plus One.py

---
+++ Plus One.py
@@ -0,0 +1,9 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        number = int("".join(map(str, digits)))
+
+        number = number + 1
+
+        answer = list(map(int, str(number)))
+
+        return answer
